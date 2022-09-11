53b0b34f3cf0e49512d07568f057f5543ee45f06
sostene <ngarukiyimanasostene@gmail.com>
2022-09-11 17:41:07 +0200

    Automated repetitive excel tasks using openpyxl package in python

---
+++ app.py
@@ -0,0 +1,30 @@
+# Automate excel file
+
+import openpyxl as xl
+from openpyxl.chart import BarChart,Reference
+
+def process_workbook(filename):
+    wb=xl.load_workbook(filename)
+    sheet = wb['Sheet1']
+
+    for row in range(2,sheet.max_row + 1):
+        cell = sheet.cell(row,3)
+
+        # change the price
+        corrected_price = cell.value * 0.9
+        corrected_price_cell = sheet.cell(row,4)
+        corrected_price_cell.value = corrected_price
+
+    #create bar chart
+    values = Reference(sheet,min_row=1,max_row=sheet.max_row,min_col=4,max_col=4)
+    chart = BarChart()
+    chart.add_data(values)
+
+    sheet.add_chart(chart,'e2')
+
+    wb.save(filename)
+
+
+process_workbook('transactions.xlsx')
+
+# start from 3:50 min 
